b627f7ebae35f25865fe9721f6dc75f881f80961
raffis <raffael.sahli@doodle.com>
2023-06-01 10:24:53 +0200

    fix: use gh cli token handler (#25)

--- cmd/main.go
+++ cmd/main.go
@@ -6,26 +6,32 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/cli/go-gh/pkg/auth"
 	"github.com/doodlescheduling/gh-bootstrap-repository/internal/repository"
 	"github.com/google/go-github/v52/github"
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
 	"golang.org/x/oauth2"
 )
 
 var (
+	host    = "github.com"
 	rootCmd = &cobra.Command{
 		Use:           "gh-bootstrap-repository [owner/repository-name] [owner/origin-repository]",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			flag.Parse()
+
 			if len(args) != 2 {
 				return errors.New("expects [owner/repository-name] [owner/origin-repository] as arguments")
 			}
 
 			name, fromRepository := args[0], args[1]
+			token, _ := auth.TokenForHost(host)
 
 			ts := oauth2.StaticTokenSource(
-				&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+				&oauth2.Token{AccessToken: token},
 			)
 
 			tc := oauth2.NewClient(context.TODO(), ts)
@@ -38,6 +44,8 @@ var (
 )
 
 func main() {
+	flag.StringVar(&host, "host", host, "The github host (github.com)")
+
 	err := rootCmd.Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error()+"\n\n")
